f72d9e1b011562eda254f054eba8372c5c00bcef
men7627 <men1219@hanmail.net>
2021-08-04 21:53:37 +0900

    tpye resolver (trivial)
    Date:      Mon Aug 3 21:44:00 2021 +0900

--- index.js
+++ index.js
@@ -4,23 +4,56 @@ const {ApolloServer} = require('apollo-server')
 const typeDefs = `
 	type Query {
 		totalPhotos: Int!
+		allPhotos: [Photo!]!
+	}
+
+	type Mutation {
+		postPhoto(name: String! description: String): Photo!
+	}
+
+	type Photo {
+		id: ID!
+		url: String!
+		name: String!
+		description: String
 	}
 `
 
+var _id = 0
+var photos = []
+
+
 const resolvers = {
 	Query: {
-		totalPhotos: () => 42
+		totalPhotos: () => photos.length,
+		allPhotos: () => photos
+	},
+
+	Mutation: {
+		postPhoto (parent, args) {
+			var newPhoto = {
+				id: _id++,
+				...args
+			}
+			photos.push(newPhoto)
+
+			return newPhoto
+		}
+	},
+
+	Photo: {
+		url: parent => `http://yoursite.com/img/${parent.id}.jpg`
 	}
 }
 
-// 2. 서버 인스턴스 새로 생성
-// 3. typeDefs(스키마)와 리졸버를 객체에 넣어 전달
+// 서버 인스턴스 새로 생성
+// typeDefs(스키마)와 리졸버를 객체에 넣어 전달
 const server = new ApolloServer({
 	typeDefs,
 	resolvers
 })
 
-// 4. 웹 서버를 구동하기 위해 listen 메서드를 호출
+// 웹 서버를 구동하기 위해 listen 메서드를 호출
 server
 	.listen()
 	.then(({url}) => console.log(`GraphQL Service running on ${url}`))
